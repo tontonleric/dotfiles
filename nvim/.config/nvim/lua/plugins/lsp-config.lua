92933589f084ad2ccb5cc374f8b56daf7d492f2d
Eric Wartelle <eric.wartelle@proton.me>
2024-06-30 13:10:47 +0200

    Ajout de la configuration d'alacritty

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "apex_ls", "lua_ls", "gitlab_ci_ls", "tsserver" }
+        ensure_installed = { "apex_ls", "lua_ls", "yamlls", "tsserver" }
       })
     end
   },
@@ -17,15 +17,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.apex_ls.setup({
-        apex_jar_path = "/home/tontonleric/apex_lsp/apex-jorje-lsp.jar",
-        apex_enable_semantic_errors = true,
-        apex_enable_completion_statistics = true,
-        settings = {
-            
-        }
-      })
-      lspconfig.gitlab_ci_ls.setup({})
+      lspconfig.apex_ls.setup({})
+      lspconfig.yamlls.setup({})
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
 
