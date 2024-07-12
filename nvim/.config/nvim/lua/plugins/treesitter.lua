aeb1e80108056aa60b25505d006a6ab94fc7a38c
Eric Wartelle <eric.wartelle@proton.me>
2024-07-12 10:15:26 +0200

    MAJ

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,25 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+      local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { "c", "lua", "apex", "soql", "sosl", "javascript", "html", "query", "toml", "python" },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
+      configs.setup({
+        ensure_installed = { "c", "lua", "apex", "soql", "sosl", "javascript", "html", "query", "toml", "python" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
 
-    vim.treesitter.language.register('apex', { 'apexcode' })
+      vim.treesitter.language.register('apex', { 'apexcode' })
 
-  end
+    end
+  }
+  ,
+  {
+    'nvim-treesitter/playground',
+    config = function()
+    end
+  }
 }
