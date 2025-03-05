8a9c9349f23e0b913c10fae40a66824463d98280
Eric Wartelle <eric.wartelle@proton.me>
2025-03-05 15:57:57 +0100

    EWA: Correction

---
+++ lazyvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.apex_ls.setup({
+        apex_jar_path = "/home/tontonleric/.local/share/nvim/mason/share/apex-language-server/apex-jorje-lsp.jar",
+      })
+      lspconfig.yamlls.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.pyright.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+
+      vim.lsp.set_log_level("debug")
+    end,
+  },
+}
--- scripts/displayhelp.sh
+++ scripts/displayhelp.sh
@@ -3,5 +3,5 @@
 ## Author      : Eric Wartelle
 ## Description : Display help
 
-rofi -e "META-F1 : Affiche l'aide" -no-config
-
+rofi -e "META-F1 : Affiche l'Affiche
+META-m : Alterne entre le mode monocle et le mode layout" -no-config
