841c19924fd05b1bb40b14eaae459c13190778d7
Eric Wartelle <eric.wartelle@proton.me>
2025-05-15 21:00:07 +0200

    EWA : Changement du thême de kitty

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
 		"williamboman/mason-lspconfig",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "apex_ls", "lua_ls"}
+				ensure_installed = { "lua_ls"}
 			})
 		end
 	},
