1fa7fd3e70964d109281f5e95f75644e907d3817
TontonLEric <tontonl3ric@protonmail.com>
2025-09-21 09:14:48 +0200

    EWA : Prise en charge du double écran dans Polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,8 +3,13 @@
 polybar-msg cmd quit
 
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-polybar bar3 2>&1 | tee -a /tmp/polybar3.log & disown
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log &
+disown
+polybar bar2 2>&1 | tee -a /tmp/polybar2.log &
+disown
+polybar bar3 2>&1 | tee -a /tmp/polybar3.log &
+disown
+polybar bar4 2>&1 | tee -a /tmp/polybar4.log &
+disown
 
 echo "Polybars launch"
