723f604c33652a335486c96587656fd70d55ea4a
Tontonl3ric <tontonl3ric@protonmail.com>
2026-07-06 07:35:13 +0200

    NVim : Spell Checking en français par défaut sur les fichiers markdown

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Spell checking in French for Markdown files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = "fr"
+  end,
+})
